3a8984a38c71d06440794394aced859c4adc24a5
James Bachrach <bachrachj@chc.edu>
2022-08-31 13:37:30 -0400

    Created the BookInStock class and application

---
+++ BookInStock.rb
@@ -0,0 +1,17 @@
+class BookInStock
+
+    attr_reader :isbn
+    attr_accessor :price
+
+    def initialize(isbn, price)
+        @isbn = isbn
+        @price = price
+    end
+
+    def to_s
+        "ISBN: #{@isbn}, Price: #{@price}"
+    end
+end
+
+b1 = BookInStock.new('isbn', 25.50)
+puts b1.to_s
