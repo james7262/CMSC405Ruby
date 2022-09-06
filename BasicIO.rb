d633bc37fb8358ddcf72d7ac6d55d30c2442b855
James Bachrach <bachrachj@chc.edu>
2022-09-06 10:50:32 -0400

    September 6th 2022

---
+++ BasicIO.rb
@@ -0,0 +1,10 @@
+# Ruby provides a whole set of I/O related methods in the Kernel module.
+# A ruby module is like a Java interface.
+
+# The print methods prints each paremeter its passed to standard out (stdout).
+# It does not append a new line to the end of its output.
+print "cat", [1,2,3], 99, "\n"
+print "cat", [1,2,3], 99, "\n"
+
+# Observations: Arguments do not need to be enclosed in parenthesis,
+# Delimeters like the semi-colon weren't needed at the end of the lines of code.
